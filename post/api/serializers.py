d1f3a8a836bfff2229bb937c3db02e3f5fda0b6c
cprm <soner@arslanyapi.com.tr>
2021-04-23 14:29:43 +0300

    post ve api islemleri yapildi

---
+++ post/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from post.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'content',
+            'image',
+            'slug',
+            'created',
+        ]
+
+
+class PostUpdateCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'content',
+            'image',
+        ]
--- post/api/views.py
+++ post/api/views.py
@@ -1,6 +1,37 @@
-from rest_framework.generics import ListAPIView
 from post.models import Post
+from post.api.serializers import PostSerializer, PostUpdateCreateSerializer
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    DestroyAPIView,
+    UpdateAPIView,
+    CreateAPIView
+)
 
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'slug'
+
+
+class PostDeleteAPIView(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'slug'
+
+
+class PostUpdateAPIView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostUpdateCreateSerializer
+    lookup_field = 'slug'
+
+
+class PostCreateAPIView(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostUpdateCreateSerializer
